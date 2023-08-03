ef5f30f4e479f8fee6af6ac46851a02b8153935c
sarancoul <sc523644@gmail.com>
2023-08-03 17:28:44 +0000

    premier commit

---
+++ src/main/java/com/gestionBudget/tpBudget/TpBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.gestionBudget.tpBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionBudget/tpBudget/TpBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionBudget.tpBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
