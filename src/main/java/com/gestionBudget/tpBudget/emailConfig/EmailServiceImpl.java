983661de1cdf46b93a7de459096a6724164fe1ea
SouleymaneFOMBA <babalarab2012@gmail.com>
2023-08-07 14:03:19 +0000

    s

--- src/main/java/com/gestionBudget/tpBudget/emailConfig/EmailServiceImpl.java
+++ src/main/java/com/gestionBudget/tpBudget/emailConfig/EmailServiceImpl.java
@@ -1,14 +1,9 @@
 package com.gestionBudget.tpBudget.emailConfig;
 
-import java.io.File;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
