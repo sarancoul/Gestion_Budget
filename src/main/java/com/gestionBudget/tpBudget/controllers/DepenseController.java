bdadb0f873efc70af59bc649c8634b5f6a83d7d7
SouleymaneFOMBA <babalarab2012@gmail.com>
2023-08-07 09:29:38 +0000

    cd

--- src/main/java/com/gestionBudget/tpBudget/controllers/DepenseController.java
+++ src/main/java/com/gestionBudget/tpBudget/controllers/DepenseController.java
@@ -5,6 +5,8 @@ import com.gestionBudget.tpBudget.services.IDepenseService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/depence")
 @AllArgsConstructor
@@ -17,6 +19,19 @@ public class DepenseController {
     private String effectuer(@RequestBody Depense depense){
         depenseService.creer(depense);
         return "Depense creer";
+    }
+    @GetMapping("/liste")
+    public List<Depense> read(){
+        return depenseService.lire();
+    }
+    @PutMapping("/modiffier/{idDepense}")
+    public Depense modiffier(@PathVariable int idDepense,@RequestBody Depense depense){
+        return depenseService.modifier(idDepense,depense);
+    }
+
+    @DeleteMapping("/supprimer/{idDepense}")
+    public String supprimer(@PathVariable Long idDepense){
+        return depenseService.supprimer(idDepense);
     }
 
 }
