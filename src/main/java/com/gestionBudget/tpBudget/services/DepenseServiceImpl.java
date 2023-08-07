dbf4d550b4bd866f3cf38a056e972af0ecd4f2a6
SouleymaneFOMBA <babalarab2012@gmail.com>
2023-08-07 09:05:47 +0000

    c

--- src/main/java/com/gestionBudget/tpBudget/services/DepenseServiceImpl.java
+++ src/main/java/com/gestionBudget/tpBudget/services/DepenseServiceImpl.java
@@ -5,6 +5,8 @@ import com.gestionBudget.tpBudget.repository.RepositoryDepense;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class DepenseServiceImpl implements IDepenseService {
@@ -15,4 +17,28 @@ public class DepenseServiceImpl implements IDepenseService {
         repositoryDepense.save(depense);
         return "Depence effectuer";
     }
+
+    @Override
+    public List<Depense> lire() {
+        return repositoryDepense.findAll();
+    }
+
+    @Override
+    public Depense modifier(int idDepense, Depense depense) {
+        return repositoryDepense.findById((long) idDepense)
+                .map(s->{
+                    s.setMontant(depense.getMontant());
+                    s.setDescription(depense.getDescription());
+                    s.setDateDebutDepense(depense.getDateDebutDepense());
+                    s.setDateFinDepense(depense.getDateFinDepense());
+                    return repositoryDepense.save(s);
+                }).orElseThrow(()->new RuntimeException("Depense non trouvé !"));
+    }
+
+    @Override
+    public String supprimer(Long idDepense) {
+        repositoryDepense.deleteById(idDepense);
+        return "Depense supprimer";
+    }
+
 }
--- src/main/java/com/gestionBudget/tpBudget/services/IDepenseService.java
+++ src/main/java/com/gestionBudget/tpBudget/services/IDepenseService.java
@@ -2,7 +2,15 @@ package com.gestionBudget.tpBudget.services;
 
 import com.gestionBudget.tpBudget.entites.Depense;
 
+import java.util.List;
+
 public interface IDepenseService {
 
     String creer(Depense depense);
+
+    List<Depense> lire();
+
+    Depense modifier(int idDepense, Depense depense);
+
+    String supprimer(Long idDepense);
 }
